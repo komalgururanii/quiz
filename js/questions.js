c713d3b792531355fe48fad8e44e7d067927f9b0
Komal Gururani <120553380+komalgururanii@users.noreply.github.com>
2024-04-04 23:21:34 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,69 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which technology is primarily responsible for the styling of web pages?",
+    answer: "CSS",
+    options: ["JavaScript", "CSS", "Python", "HTML"],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "What is the purpose of a front-end web development framework like React or Angular?",
+    answer: " To create a visually appealing user interface",
+    options: [
+      "To manage databases and server-side logic",
+      " To create a visually appealing user interface",
+      "To handle server-side routing",
+      "To handle server-side routing",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What is the purpose of the script tag in HTML?",
+    answer: "To include external JavaScript code",
+    options: [
+      "To define the page’s structure",
+      "To include external CSS styles",
+      "To include external JavaScript code",
+      "To create hyperlinks",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
